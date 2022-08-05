d3bd0742e69cddce46f00172671f4d72e0db189f
zzztttkkk <ztkisalreadytaken@gmail.com>
2022-08-05 10:31:54 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@ mod h2tp;
 #[tokio::main]
 async fn main() {
 	let mut server = h2tp::create_server();
-	server.listen("127.0.0.1:8080").await;
+	tokio::spawn(async move {
+		server.listen("127.0.0.1:8080").await;
+	});
+
+	match tokio::signal::ctrl_c().await {
+		Ok(()) => {}
+		Err(err) => {
+			eprintln!("Unable to listen for shutdown signal: {}", err);
+		}
+	}
 }
