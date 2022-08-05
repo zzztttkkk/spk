efb25b6247e773660f83e53b63054f8c76c2fee7
zzztttkkk <ztkisalreadytaken@gmail.com>
2022-08-05 10:23:34 +0800

    -

--- src/h2tp/server.rs
+++ src/h2tp/server.rs
@@ -13,8 +13,9 @@ impl Server {
 	pub async fn listen<Addr: tokio::net::ToSocketAddrs>(&mut self, addr: Addr) {
 		self.listener = Some(TcpListener::bind(addr).await.unwrap());
 
+		let lref = self.listener.as_ref().unwrap();
 		loop {
-			let (stream, _) = self.listener.as_mut().unwrap().accept().await.unwrap();
+			let (stream, _) = lref.accept().await.unwrap();
 			tokio::spawn(async move {
 				let mut conn = Conn::new(stream);
 				conn.handle().await;
